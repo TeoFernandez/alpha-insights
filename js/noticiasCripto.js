fe15b0cacae0210b5ab8c41b496e8d52f5c8d0ac
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-06-16 16:53:31 -0300

    Actualizar título de sección de noticias y definir constantes para la API de noticias sobre criptomonedas

--- js/noticiasCripto.js
+++ js/noticiasCripto.js
@@ -1,3 +1,9 @@
-const API_KEY = "WPHHLLBSYQ0AXUFD"
-const FUNCTION = "NEWS_SENTIMENT"
-const respuesta = await fetch()
+//https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=COIN,CRYPTO:BTC,FOREX:USD&sort=LATEST&limit=5&apikey=WPHHLLBSYQ0AXUFD
+
+const URL = "https://www.alphavantage.co/query?";
+const API_KEY = "WPHHLLBSYQ0AXUFD";
+const FUNCTION = "NEWS_SENTIMENT";
+const TICKERS = "COIN,CRYPTO:BTC,FOREX:USD";
+const SORT = "LATEST";
+const LIMIT = 5;
+
